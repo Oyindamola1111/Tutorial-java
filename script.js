269e26b14cfa708bdfeeeafe17e0114d45037bc3
Oyindamola1111 <ajalajohnson27@gmail.com>
2026-03-12 11:12:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,271 @@
+// //  PASSENGER COUNTER APP
+// let countEl = document.getElementById("count-el")
+// let saveEl = document.getElementById("save-el")
+// let showEl = document.getElementById("show-el")
+
+// let count = 0
+// console.log(count);
+
+// function increment() {
+//   count = count + 1
+//   countEl.textContent = count
+//   showEl.innerText =  "✅ " + count 
+//   console.log(count);
+// }
+
+// function save() {
+//   let savings = count + " - "
+//  saveEl.textContent += savings
+//  countEl.textContent = 0
+//  count = 0
+// }
+
+//  PASSENGER COUNTER APP ENDED
+
+// E-COMMERCE ERROR MESSAGE APP
+
+// let errorEl = document.getElementById("error-el")
+
+// function purchase() {
+//     errorEl.innerText = "something went wrong, please try again"
+// }
+
+// E-COMMERCE ERROR MESSAGE APP ENDED
+
+// MINI CALCULATOR
+let num1 = 8
+let num2 = 2
+
+let num1El = document.getElementById("num1-el").textContent = num1
+let num2El = document.getElementById("num2-el").textContent = num2
+let sumEl = document.getElementById("sum-el")
+
+
+function addition() {
+   let result = num1 + num2
+   sumEl.textContent ="sum:" + result
+    
+}
+
+function subtraction() {
+    let result = num1 - num2
+    sumEl.textContent = "sum:" + result   
+}
+
+function division() {
+    let result = num1 / num2
+    sumEl.textContent = "sum:" + result
+}
+
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "sum:" + result
+}
+
+// MINI CALCULATOR ENDED
+
+
+// let firstName = "Daniel"
+// let secondName = "poland"
+// let fullName = firstName + " " + secondName
+// console.log(fullName);
+
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// function myGreeting() {
+// let welcome = greeting +", "+ name +"!"
+// console.log(welcome);
+// }
+//  myGreeting()
+
+
+ 
+// let myPoints = 3
+
+// function add3Points() {
+//   myPoints = myPoints + 3
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+
+// function remove1Point() {
+//   myPoints = myPoints - 1
+// }
+// remove1Point()
+// remove1Point()
+
+// console.log(myPoints);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let countEl = document.getElementById("count-el")
+// let saveEl = document.getElementById("save-el")
+// let displayEl = document.getElementById("display-el")
+
+
+// let count = 0
+
+// function increment() {
+//   count += 1
+//   countEl.innerText = count
+//   displayEl.innerText = countEl.innerText
+//   console.log(count);
+// }
+
+// function save() {
+//  let entry = count + "- " 
+//   saveEl.innerText += entry
+// }
+
+// function remove() {
+// let erase = count
+// saveEl.textContent += erase
+
+// console.log(erase);
+// }
+
+
+
+
+
+
+// console.log(4+5)
+// console.log("2"+"4");
+// console.log("5"+1);
+// console.log(100+"100");
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let name = "Dami"
+// let greetings = "Welcome back"
+// welcomeEl.innerText = greetings + " " + name 
+// welcomeEl.innerText += "👋🏼"
+
+
+// let name = "Sanusi Oyindamola"
+// let greeting = "Hi, my name is"
+// let myGreeting = greeting + " " + name
+// console.log(myGreeting);
+
+// let name = "Dami"
+// let message = "you have three new notifications"
+// let messageToUser = name + ", " + message
+// console.log(messageToUser)
+
+
+
+// let lapsCompleted = 0
+
+// function increment() {
+// lapsCompleted = lapsCompleted + 1
+// }
+
+// increment()
+// increment()
+// increment()
+
+// console.log(lapsCompleted)
+
+// let lap1 =34
+// let lap2 = 33
+// let lap3 = 36
+// let sum = lap1 + lap2 + lap3
+
+// function totalLapTime() {
+//    console.log(sum) 
+// }
+// totalLapTime() 
+
+// function countDown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+// countDown()
+
+// function callNumber() {
+//     console.log(42)
+// }
+// callNumber()
+
+
+//  let myAge =31
+//  let humanDogRatio = 7
+//  let myDogAge = myAge * humanDogRatio
+//  console.log(myDogAge)
+
+// const name = "hello world" 
+// console.log(name)
+
+// let count = 4
+// count += 1
+// console.log(count)
+
+// let bonusPoints = 50
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+
+// bonusPoints=bonusPoints + 45
+// console.log(bonusPoints)
+
+// let myAge = 28
+// console.log(myAge)
+
+
+//COUNTER PASENGER APP
+
+// document.getElementById("count-el").innerText=5
+
+//COUNTER PASENGER APP ENDED
