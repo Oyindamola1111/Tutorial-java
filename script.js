d18c01f52bfe32eee2d238b0d05ab81020534cb8
Oyindamola1111 <ajalajohnson27@gmail.com>
2026-03-19 09:38:28 +0100

    card enhancement

--- script.js
+++ script.js
@@ -64,50 +64,118 @@
 
 
 // BLACKJACK GAME APP
-// let messageEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
-// let cardEl = document.getElementById("cards-el")
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardEl = document.getElementById("cards-el")
 
-// let firstCard = 10
-// let secondCard = 11
-// let sum = firstCard + secondCard
-// let cards = [firstCard,secondCard ]
-// let hasBlackJack = false
-// let isAlive = true
-// console.log(sum);
-// let message = " "
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = " "
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random()* 13) + 1
+     if (randomNumber > 10) {
+       return 11
+    }else if (randomNumber === 1) {
+       return 10
+    }
+    return randomNumber
+}
+
+function startGame() {
+isAlive = true
+let firstNumber = getRandomCard()
+let secondNumber = getRandomCard()
+sum = firstNumber + secondNumber
+cards=[firstNumber,secondNumber]
+
+   renderGame() 
+}
 
 
-// function startGame() {
-//    renderGame() 
-// }
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum
+  cardEl.textContent = "Cards: "
 
-// function renderGame() {
-//   sumEl.textContent = "Sum: " + sum
-//   cardEl.textContent = "Cards: " + cards[0] +" - " + cards[1]
+  for (let i = 0; i < cards.length; i += 1) {
+    cardEl.textContent += cards[i] + " "
+  }
 
-//     if (sum <= 20) {
-//         message = "draw a new card"
-//     }
-//     else if (sum === 21) {
-//         message = "you won a blackjack huray"
-//         hasBlackJack = true  
-//     }
-//     else {
-//         message = "you are out of the game"
-//         isAlive = false
-//     }
+    if (sum <= 20) {
+        message = "draw a new card"
+    }
+    else if (sum === 21) {
+        message = "you won a blackjack huray"
+        hasBlackJack = true  
+    }
+    else {
+        message = "you are out of the game"
+        isAlive = false
+    }
     
-//     messageEl.textContent = message
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+}
+
+// BLACKJACK GAME APP ENDED 
+
+
+
+
+
+// function rollDice() {
+//     let randomNumber = Math.floor(Math.random()*6 ) +1
+//     return randomNumber
+    
+// }
+// console.log(rollDice())
+
+// let race1 = 200
+// let race2 = 300
+
+// function totalRaceTime() {
+//      return race2
+// }
+// let allRace = totalRaceTime()
+// console.log(allRace)
+
+// let cards = [2,3,6,4]
+
+// for (let i = 0; i < cards.length; i += 1) {
+//     console.log(cards[i])
 // }
 
-// function newCard() {
-//     let card = 5
-//     sum += card
-//     renderGame()
+// let sentence = ["hello","my","name","is","per"]
+// let greetingEl = document.getElementById("greeting-el")
+
+// for (let i = 0; i < sentence.length; i += 1) {
+//    greetingEl.textContent += sentence[i] + " "
 // }
 
-// BLACKJACK GAME APP ENDED
+// let myProperties =[
+//     "zero experience",
+//     "bsc",
+//     "no licenses",
+//     "footballer"
+// ]
+
+// let newProperties = "i am alive"
+// myProperties.push(newProperties)
+// console.log(myProperties)
+
+// console.log(myProperties[2])
+// console.log(myProperties[0])
+// console.log(myProperties[3])
+// console.log(myProperties[1])
 
 // jhjhjhj
 
